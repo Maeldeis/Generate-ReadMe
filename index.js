b2388c812143f45972daf1592437383846f68a85
Marta <maeldeis@gmail.com>
2024-02-18 15:06:10 +0100

    used async/await syntax to handle promises, added error handling and logged errors, defined init function to prompt user questions and defined writetofile function to new path

--- index.js
+++ index.js
@@ -4,28 +4,28 @@ import inquirer from "inquirer";
 import { questions } from "./utils/questions.js";
 import { generateMarkdown } from "./utils/generateMarkdown.js";
 
+const writeToFile = async (fileName, data) => {
+  const filePath = path.resolve("./utils", fileName); 
 
-const writeToFile = (fileName, data) => {
-
-  const filePath = path.resolve("./generatedMarkdown", fileName); 
-
-  fs.writeFile(filePath, data, err => {
-    if (err) throw err;
-  });
-}
+  try {
+    await fs.writeFile(filePath, data);
+    console.log("File written successfully:", filePath);
+  } catch (err) {
+    console.error("Error writing file:", err);
+  }
+};
 
 const init = async () => {
   try {
     const answers = await inquirer.prompt(questions);
     const markdownContent = generateMarkdown(answers);
 
-    writeToFile("README.md", markdownContent); 
+    await writeToFile("README.md", markdownContent); 
 
     console.log("Successfully generated!");
   } catch (error) {
     console.error("Error occurred:", error);
   }
-}
-
+};
 
 init();
